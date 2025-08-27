7c03e5ce64cf9b57bdea3b22a917b26def61fcad
vaishnav sagaonkar <vaishnavsagaonkar112233@gmail.com>
2025-08-27 11:38:01 +0530

    initial commit 5

--- router/user.js
+++ router/user.js
@@ -3,10 +3,20 @@ const { handlecreateuser, handlelogin } = require("../controllers/user");
 
 const router = express.Router();
 
-// Signup
+// Show signup page
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// Handle signup
 router.post("/signup", handlecreateuser);
 
-// Login
+// Show login page
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Handle login
 router.post("/login", handlelogin);
 
 module.exports = router;
